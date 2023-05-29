5bed0819b3a37dd7e94d45248820c3f45d5f6652
rsellami <r.sellami@altereo.fr>
2023-05-29 22:01:42 +0200

    should retun 'jean\nluc' when paragraph is 'jean luc' and column number is 5 (refacto)

--- src/wordWrap.ts
+++ src/wordWrap.ts
@@ -1,9 +1,10 @@
 export function wrapWord(paragraph: string, columnNumber: number): string {
     if (paragraph.length > columnNumber) {
         const space = ' ';
+        const breakLine = '\n';
         let endColumNumber = paragraph[columnNumber - 1] === space ? columnNumber - 1 : columnNumber;
         let nextStartColumnNumber = paragraph[columnNumber] === space ? columnNumber + 1 : columnNumber;
-        paragraph = paragraph.substring(0, endColumNumber) + '\n' +
+        paragraph = paragraph.substring(0, endColumNumber) + breakLine +
             wrapWord(paragraph.substring(nextStartColumnNumber), columnNumber);
     }
     return paragraph;
