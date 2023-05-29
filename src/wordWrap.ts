1c48e4a61ec57a9704f8718eb41d3c1c9562837f
rsellami <r.sellami@altereo.fr>
2023-05-29 21:30:18 +0200

    should retun 'x\nx' when paragraph is 'xx' and column number is 1 (green)

--- src/wordWrap.ts
+++ src/wordWrap.ts
@@ -1,3 +1,6 @@
 export function wrapWord(paragraph:string, columnNumber: number): string {
+    if(paragraph.length>columnNumber){
+        paragraph = paragraph.substring(0,columnNumber)+'\n'+paragraph.substring(columnNumber);
+    }
     return paragraph;
 }
