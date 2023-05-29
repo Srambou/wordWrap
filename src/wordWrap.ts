a820197b8942b2acadc63f308fca286c40713115
rsellami <r.sellami@altereo.fr>
2023-05-29 21:52:43 +0200

    should retun 'jean\nluc' when paragraph is 'jean luc' and column number is 4 (green)

--- src/wordWrap.ts
+++ src/wordWrap.ts
@@ -1,7 +1,9 @@
 export function wrapWord(paragraph: string, columnNumber: number): string {
     if (paragraph.length > columnNumber) {
-        paragraph = paragraph.substring(0, columnNumber) + '\n' +
-            wrapWord(paragraph.substring(columnNumber), columnNumber);
+        let endColumNumber = columnNumber;
+        let nextStartColumnNumber = paragraph[columnNumber] === ' ' ? columnNumber + 1 : columnNumber;
+        paragraph = paragraph.substring(0, endColumNumber) + '\n' +
+            wrapWord(paragraph.substring(nextStartColumnNumber), columnNumber);
     }
     return paragraph;
 }
